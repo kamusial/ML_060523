3edc2670e817d3887bd8528bb7ef55e551d996e5
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-06 14:51:36 +0200

    1

--- 6_siaka_parametrow.py
+++ 6_siaka_parametrow.py
@@ -3,6 +3,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv('heart.csv', comment='#')
 print(df.head(10).to_string())
@@ -23,6 +24,20 @@ drzewo(3)
 drzewo(5)
 drzewo(10)
 
+print('\nLogistyczna regresja')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+import joblib
+joblib.dump(model, 'Log_reg_v2.3.model')
+model_kamila = joblib.load('Log_reg_v2.3.model')
+
+print(dir(model_kamila))
+print(model_kamila._estimator_type, model_kamila.get_params)
+
+
 model = DecisionTreeClassifier()
 params = {
     'max_depth': range(2, 15),
@@ -32,12 +47,12 @@ params = {
     'criterion': ['gini', 'entropy', 'log_loss']
 }
 
-grid = GridSearchCV(model, params, scoring='accuracy', cv=10, verbose=1)
-grid.fit(X_train, y_train)
-
-print(grid.best_params_)
-print(grid.best_score_)
-print(grid.best_estimator_)
-print(pd.DataFrame(grid.best_estimator_.feature_importances_,X.columns).sort_values(0, ascending=False))
-y_pred = grid.best_estimator_.predict(X_test)
-print(pd.DataFrame(confusion_matrix(y_test, y_pred)))
+# grid = GridSearchCV(model, params, scoring='accuracy', cv=10, verbose=1)
+# grid.fit(X_train, y_train)
+#
+# print(grid.best_params_)
+# print(grid.best_score_)
+# print(grid.best_estimator_)
+# print(pd.DataFrame(grid.best_estimator_.feature_importances_,X.columns).sort_values(0, ascending=False))
+# y_pred = grid.best_estimator_.predict(X_test)
+# print(pd.DataFrame(confusion_matrix(y_test, y_pred)))
