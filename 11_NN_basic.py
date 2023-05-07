bc13bf4bc68da0e9e116fc2c99aa081d181d030e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-07 13:40:14 +0200

    1

---
+++ 11_NN_basic.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+#komponenty do budowy sieci
+from tensorflow.keras.models import Sequential   #szkielet sieci
+from tensorflow.keras.layers import Dense
+from tensorflow.random import set_seed
+
+set_seed(0)
+model = Sequential()
+model.add()
